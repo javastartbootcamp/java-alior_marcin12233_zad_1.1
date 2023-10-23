d2cd51b25b0878e186001ec47784d52f38f49680
Marcin <mkudyba421@gmail.com>
2023-10-23 20:45:26 +0200

    second subtask

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,5 +4,7 @@ public class App {
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
     }
 }
