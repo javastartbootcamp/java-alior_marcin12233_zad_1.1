77d1046fd3e8473674e3f0872c527d09853c38e9
Marcin <mkudyba421@gmail.com>
2023-10-23 20:44:40 +0200

    first subtask

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+public class App {
+
+    public static void main(String[] argc) {
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+    }
+}
